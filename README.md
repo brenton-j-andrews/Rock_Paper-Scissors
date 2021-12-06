37d12a2d459194cfd2a57f0db8f33b369831ec6c
brent <brenton.j.andrews@gmail.com>
2021-12-06 08:51:00 -0700

    UI added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Rock_Paper-Scissors
-A rock, paper, scissors web game in javascript done for The Odin Project
+A rock, paper, scissors web game in javascript done for The Odin Project. I was going to add some animations but then I lost motivation. One problem to fix in the future is the alignment of the "play again" button that appears after each game, it is currently skewed to the left. 
+
+Image credits:
+Icons sourced from www.icons8.com, 
+Background image from assetstore.unity.com
--- script.js
+++ script.js
@@ -3,17 +3,29 @@ let computer_score = 0;
 let player_move = null;
 const moves = {0: "Rock", 1: "Paper", 2: "Scissors"};
 
+// Generate computer move.
 function computerPlay() {
     let moves = [0, 1, 2];
     let rand_index = Math.floor(Math.random() * 3);
     return Number(moves[rand_index]);
 }
 
+// Play one round of R.P.S and handle images / animations.
 function playRound(playerMove, computerMove) {
-    console.log("Player plays: " + Number(playerMove));
-    console.log("Computer plays: " + computerMove);
+
+    document.getElementById("player_text").textContent = `You played ${moves[playerMove]}!`
+    console.log(playerMove);
+    console.log(computerMove);
+    let player_img = "images/battle_images/battle_" + moves[playerMove] + ".jpeg";
+    let computer_img = "images/battle_images/battle_" + moves[computerMove] +"_reversed.jpeg";
+    document.getElementById("player_img").src = player_img;
+    document.getElementById("computer_img").src = computer_img;
+
+    console.log(computer_img);
+    document.getElementById("computer_text").textContent = `Opponent plays ${moves[computerMove]}!`
     let outcome_arr = [[0, 1, 2], [2, 0, 1], [1, 2, 0]];
-    let result = outcome_arr[player_move][computerMove];
+    let result = outcome_arr[computerMove][playerMove];
+    console.log(result);
     if (result === 0) {
         console.log("Its a tie!");
         return;
@@ -27,6 +39,7 @@ function playRound(playerMove, computerMove) {
     updateScore();
 }
 
+// Update html scoreboard. Used to avoid repitition.
 function updateScore() {
     document.getElementById("player").innerHTML = `Player Score: ${player_score}`;
     document.getElementById("computer").innerHTML = `Computer Score: ${computer_score}`;
@@ -42,14 +55,24 @@ function gameOver() {
             document.getElementById("winner").innerHTML = "You loose, better luck next time!";
         }
         buttons.forEach((button) => button.disabled = true);
+
+
+        // Reset images and text on the arena screen if "play_again" button pressed.
         let play_again = document.createElement("button");
-        play_again.textContent = "Play again?"
+        play_again.textContent = "Play again?";
+        play_again.className = "again_button";
         play_again.addEventListener('click', () => {
             player_score = 0;
             computer_score = 0;
             updateScore();
             buttons.forEach((button) => button.disabled = false);
             play_again.remove();
+            document.getElementById("winner").innerHTML = "Who will win?"
+            document.getElementById("player_img").src = null;
+            document.getElementById("computer_img").src = null;
+            document.getElementById("computer_text").textContent = null;
+            document.getElementById("player_text").textContent = null;
+
         });
         div.appendChild(play_again);
     }
