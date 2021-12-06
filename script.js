dc8e0d5c0fad9b4e6d75f94611f26e3ba8ac96f6
brent <brenton.j.andrews@gmail.com>
2021-12-06 08:50:39 -0700

    UI added

--- script.js
+++ script.js
@@ -1,67 +1,65 @@
+let player_score = 0;
+let computer_score = 0;
+let player_move = null;
+const moves = {0: "Rock", 1: "Paper", 2: "Scissors"};
+
 function computerPlay() {
-    let moves = ["rock", "paper", "scissors"];
+    let moves = [0, 1, 2];
     let rand_index = Math.floor(Math.random() * 3);
-    return moves[rand_index];
+    return Number(moves[rand_index]);
 }
 
 function playRound(playerMove, computerMove) {
-    console.log("Player plays: " + playerMove);
+    console.log("Player plays: " + Number(playerMove));
     console.log("Computer plays: " + computerMove);
-    let victory = null;
-
-    if (playerMove === "scissors" && computerMove === "paper") {
-        victory = true;
-    } else if (playerMove === "scissors" && computerMove === "rock") {
-        victory = false;
-    } else if (playerMove === "rock" && computerMove === "scissors") {
-        victory = true;
-    } else if (playerMove === "rock" && computerMove === "paper") {
-        victory = false;
-    } else if (playerMove === "paper" && computerMove === "rock") {
-        victory = true;
-    } else if (playerMove === "paper" && computerMove === "scissors") {
-        victory = false;
-    } 
-
-    if (victory) {
-        console.log(`You win! ${playerMove} beats ${computerMove}.`);
-    } else if (victory == false) {
-        console.log(`You lose... ${computerMove} beats ${playerMove}.`);
-    } else {
+    let outcome_arr = [[0, 1, 2], [2, 0, 1], [1, 2, 0]];
+    let result = outcome_arr[player_move][computerMove];
+    if (result === 0) {
         console.log("Its a tie!");
-    } 
-
-    return victory;
-
+        return;
+    } else if (result === 1) {
+        console.log(`You win! ${moves[playerMove]} beats ${moves[computerMove]}!`);
+        player_score++;
+    } else if (result === 2) {
+        console.log(`You loose... ${moves[computerMove]} beats ${moves[playerMove]}!`);
+        computer_score++;
+    }
+    updateScore();
 }
 
-function game() {
-    let player_wins = 0;
-    let computer_wins = 0;
-    let outcome = null;
+function updateScore() {
+    document.getElementById("player").innerHTML = `Player Score: ${player_score}`;
+    document.getElementById("computer").innerHTML = `Computer Score: ${computer_score}`;
+}
 
-    // Iterate over game sequence 5 times.
-    for (let i = 0; i < 5; i++) {
-        playerMove = prompt("Choose 'Rock', 'Paper', 'Scissors': ").toLowerCase();
-        outcome = playRound(playerMove, computerPlay());
-
-        if (outcome) {
-            player_wins++;
-        } else if (outcome == false) {
-            computer_wins++;
+// Disable turn buttons, create "play again" button and reset scores.
+function gameOver() {
+    if (player_score == 5 || computer_score == 5) {
+        let div = document.getElementById("game_over");
+        if (player_score == 5) {
+            document.getElementById("winner").innerHTML = "You win! Congratulations!";
+        } else {
+            document.getElementById("winner").innerHTML = "You loose, better luck next time!";
         }
-        console.log(`Current score: Player ${player_wins}, computer ${computer_wins}`)
+        buttons.forEach((button) => button.disabled = true);
+        let play_again = document.createElement("button");
+        play_again.textContent = "Play again?"
+        play_again.addEventListener('click', () => {
+            player_score = 0;
+            computer_score = 0;
+            updateScore();
+            buttons.forEach((button) => button.disabled = false);
+            play_again.remove();
+        });
+        div.appendChild(play_again);
     }
-
-    // Compare final player_wins and computer_wins score counts to determine a winner, if there is one.
-    if (player_wins > computer_wins) {
-        console.log(`Final score: ${player_wins} vs ${computer_wins}. Player wins!`)
-    } else if (computer_wins > player_wins) {
-        console.log(`Final score: ${computer_wins} vs ${player_wins}. Computer wins!`)
-    } else {
-        console.log(`Final score: ${computer_wins} vs ${player_wins}. It is a tie!`)
-    }
-
 }
 
-game();
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        player_move = Number(button.id);
+        playRound(player_move, computerPlay());
+        gameOver();
+    });
+});
